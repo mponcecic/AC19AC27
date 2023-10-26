8d9639301d4b49a6ce573fa717f617a1087d0fa5
María Ponce Rodríguez <mponce@cicbiogune.int>
2023-10-26 10:56:51 +0200

    Sample information script

--- Scripts/00_Sample_Info.R
+++ Scripts/00_Sample_Info.R
@@ -7,19 +7,22 @@
 # Summary
 # --------
 #
-# Generate a sample information file also refered as metadata. The sample 
+# Generate a sample information file also refereed as metadata. The sample 
 # information file is used since the beginning of the analysis to generate 
 # different scripts and in the differential expression analysis which is based 
 # in the project information. Allowing the execution of the scripts with minor 
 # adjustments.
 # 
-# This file must be MANUALLY adjusted for the different projects. 
+# This file must be MANUALLY adjusted for the different projects. However, part 
+# of the information is rescued from the pdf file with the library preparation 
+# information. 
 # 
 # Content
 #   - Named Sample_info.csv
 #   - Must have at least, the following variables:
-#     - Sample: Variable with the samples names 
-#     - Treatment: Specific treatment per sample. Must be a factor variable.
+#     - Sample: Variable with the samples names. MUST NAME Sample
+#     - Treatment: Specific treatment per sample. Must be a factor variable, can 
+#       be named as you prefer but must be consisting for the following analysis
 
 
 
@@ -36,6 +39,7 @@ suppressMessages(library(tidyverse))
 #                                 LOAD DATA                         
 ################################################################################
 
+
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Project name
 project <- "AC58"
@@ -44,8 +48,19 @@ project <- "AC58"
 # Can be your personal folder in BigData
 path <- "W:/mponce/"
 
-# Input directory. Raw gene counts  
+# Input directory
+# Must be the folder where a the library preparation pdf is found and folder 
+# with the fastq are included 
 dir_in <- "W:/DATA_shared/AC-58_TotalRNAseq/"
+
+# Last sample found in the Library Preparation pdf
+last_sample <- "AC-58_L16"
+
+# Condition
+trt <- "Time"
+  
+# Contrast level order 
+lvl_order <- c("Control", "4", "24", "48")
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Output directory
@@ -57,36 +72,51 @@ setwd(dir_out)
 # List of samples names
 samples <- unique(gsub("_1.fastq.gz","", list.files(path = paste(dir_in, "FASTQs/", sep = ""), pattern = "_1.fastq.gz")))
 
-file <- list.files(path = dir_in, pattern = "Library_Preparation")
+# PDF file with the library preparation report
+# This file should be in the project DATA_shared folder 
+file <- pdf_text(paste(dir_in, list.files(path = dir_in, pattern = "Library_Preparation"), sep = ""))
 
 
-file <- pdf_text(paste(dir_in, file, sep = ""))
+################################################################################
+#                             FUNCTION                           
+################################################################################
 
 
-# https://www.youtube.com/watch?v=bJH-S2iaxNo&ab_channel=DataCentricInc.
-clean_tab <- function(x, last_sample){
+# From pdf to data frame
+pdf_to_tab <- function(x, last_sample){
+  
+  # Description
+  # 
+  # Transform the table from the pdf into a data frame to select the variables of 
+  # interest
+  # This can not be completely automatized because the tables differ from projects
+  
+  # Create a character vector using \n as the row separator
   x <- map(x, ~ str_split(.x, "\\n") %>% unlist())
   x <- reduce(x, c)
   
-  tab_start <- str_which(tolower(x), "library id")[1]
+  # Select the beginning of the table
+  # Must be always the same
+  if(is.na(str_which(x, "Kibrary ID")[1])){
+    tab_start <- str_which(x, "GAP ID")[1]
+  } else {tab_start <- str_which(x, "Library ID")[1]}
+  
+  # Select the row of the end
   tab_end <- str_which(x, last_sample)[1]
   
-  tab_end <- tab_end[min(which(tab_end > tab_start))]
-  
+  # Select the rows of interest
   tab <- x[(tab_start):(tab_end)]
+  
+  # Change column separator form spaces to "|" and remove the first four rows
   tab <- str_replace_all(tab, "\\s{2,}", "|")
   tab <- tab[-(1:4)]
-
-  tab_df <- as.data.frame(do.call(rbind, strsplit(tab, "|", fixed = TRUE)))[,-1]
   
-  colnames(tab_df) <- c("LibraryID", "GAPID", "SampleID", "Sample", "Organism", "Group", "QUBIT", "RIN")
+  # Create a data frame of the table using "|" as a separator 
+  # Remove the first column beacuse it's empty
+  tab_df <- as.data.frame(do.call(rbind, strsplit(tab, "|", fixed = TRUE)))[,-1] 
   
-  res <- tab_df %>% select(Sample, Organism, RIN)
-  
-  return(res)
+  return(tab_df)
 }
-
-clean_tab(file, "AC-58_L16")
 
 
 ################################################################################
@@ -94,22 +124,34 @@ clean_tab(file, "AC-58_L16")
 ################################################################################
 
 
+# Run the function
+data <- pdf_to_tab(file, last_sample)
+print(head(data))
+
 # Number of samples
 n <- length(samples)
 
-# Sample names
-sample_names <- str_sort(samples, numeric = TRUE)
+# Verification
+ifelse(nrow(data) == n, print("The number of samples from the data table in the pdf match the samples in the folder"), paste("ERROR: Samples do not match", "CHANGE THE NUMBER OF ROWS REMOVED IN THE FUNCTION", sep = "\n."))
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# Sample information
-sample_info <- data.frame(
-  Sample = sample_names,
-  Time = c(rep("4", 4), rep("24", 4), rep("48", 4), rep("Control", 4)),
-  RIN = c("10","10","10","10","10","10","10","10","10","10","10","9.9","10","10","10","10"),
-  Organism = rep(x = "Homo sapiens", n),
-  Ind = c("E1","E2","E3","E4","E1","E2","E3","E4","E4","E1","E2","E3","E2","E3","E4","E1"))
-#----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Select manually the columns because theorder is not the same in all the pdfs
+# Usually select 
+#  - Sample names compatible with the file names
+#  - Organism
+#  - RIN values
+data <- data %>% select(V5, V6, V9) %>% arrange(desc(V5))
+colnames(data) <- c("Sample", "Organism", "RIN")
 
+# Add the condition variable
+# Be careful adding the comparison levels
+data[trt] <- factor(c(rep("Control", 4), rep("4", 4), rep("48", 4), rep("24", 4)), levels = lvl_order, ordered = TRUE)
+
+# Optional
+# Some sequencing projects migth include information from different tissues, projects, ...
+# You can add this information as a new variable or generate two separet csv files
+data <- data %>% mutate(Ind = sub(".*E", "E", data$Sample)) %>% arrange(Time)
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 #######################################################################
