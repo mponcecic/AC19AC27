ca3d23534fad48aa0cd6e7828ad172506c4cbd8c
María Ponce Rodríguez <mponce@cicbiogune.int>
2023-11-21 09:23:29 +0100

    Create verification directory

--- Verification_scripts/00_fastq_file.R
+++ Verification_scripts/00_fastq_file.R
@@ -12,12 +12,19 @@
 #                         LOAD FILES AND DATA                           
 ################################################################################
 
-
+# -------------------------------------------------------------------------------------------------------------------------------
+# Project name 
+project <- "XXX"
+  
 # Input directory
-input_dir <- "W:/DATA_shared/AC-58_TotalRNAseq/FASTQs/"
+input_dir <- "W:/DATA_shared/XX_mRNAseq/FASTQs/"
 # Output directory
-output_dir <- "W:/mponce/AC58/VERIFICATION"
+path <- "W:/mponce/"
+# -------------------------------------------------------------------------------------------------------------------------------
 
+dir.create(file.path(paste(path, project, "/VERIFICATION", sep = "")))
+dir_out <- paste(path, project, "/VERIFICATION", sep = "")
+  
 # Set working directory
 setwd(input_dir)
 
@@ -56,5 +63,5 @@ for (i in 1:length(samples_names)){
 ################################################################################
 
 # Export to *.csv file on the personal folder
-write.csv(def_file, file = paste(output_dir,"/00_Fastq.csv", sep = ""), row.names = FALSE)
+write.csv(def_file, file = paste(dir_out, "/00_Fastq.csv", sep = ""), row.names = FALSE)
 
