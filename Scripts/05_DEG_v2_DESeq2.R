673df3eb152ac5168721ca6a849fd4217759b799
María Ponce Rodríguez <mponce@cicbiogune.int>
2023-11-29 10:03:23 +0100

    Adjust to changes in 05_DEG_v1_qc

--- Scripts/05_DEG_v2_DESeq2.R
+++ Scripts/05_DEG_v2_DESeq2.R
@@ -50,7 +50,7 @@
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Project name
-project <- "XXX"
+project <- "AC70"
 
 # Pathway to the folders and files
 # Select one option depending if you are running the script in Rocky or local
@@ -58,7 +58,7 @@ project <- "XXX"
 path <- "W:/mponce/"
 
 # Date of the log file 5_DEG_qc_XXXX.log
-logdate <- "202311"
+logdate <- "20231128"
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Load libraries
@@ -110,9 +110,17 @@ contrast <- split(contrast, rep(1:(length(contrast)/3), each = 3))
 if(is.na(logfile$Outliers)){outliers <- NULL} else {outliers <-  paste(logfile$Outliers, collapse = ",")}
 
 
-# Filtering lowly expressed genes
-# Default: TRUE
-filter_cutoff <- logfile$filter_cutoff
+# Filtering parameters 
+
+# Minimum number of counts per gene
+min_total <- logfile$min_total
+# Minimum number of counts per sample
+min_count <- logfile$min_count
+# Large sample size 
+n_large <- logfile$n_large
+# Proportion
+min_prop <- logfile$min_prop
+
 
 
 ### Threshold criteria 
@@ -151,6 +159,8 @@ specie <- logfile$Organism
 # Method used to study DEGs
 analysis <- "DESeq2"
 
+# Variance transformation method used 
+md <- logfile$Variance
 
 
 ################################################################################
@@ -159,7 +169,7 @@ analysis <- "DESeq2"
 
 
 ## Load metadata file 
-sample_info <- read.table(paste(dir_output, "/Metadata_", project, ".txt", sep = ""))
+sample_info <- read.table(paste(dir_out, "/Metadata_", project, ".txt", sep = ""))
 
 ## Load annotation data
 # Genome annotation path
@@ -167,10 +177,13 @@ anot_path <- sub(pattern = "/.*", replacement = "", path)
 if (anot_path == ""){anot_path <- "/vols/GPArkaitz_bigdata"}
 # Reference genome
 ref_genome <- read.table(paste(anot_path, "/DATA_shared/Genomes_Rocky/DEG_Annotation/Annotated_Genes_20231121.txt", sep = ""), header = TRUE)
-genome <- ref_genome[which(ref_genome$Specie == specie), -3]
+genome <- ref_genome[which(ref_genome$Specie == specie), -4]
 genome$Name <- paste(genome$Symbol, genome$Ensembl, sep = "_")
 print(dim(genome))
 
+
+## Load transformed data 
+m_vst <- read.table(file = paste(dir_infiles, "GeneCount_filter_", md, "_blindFALSE_", project, ".txt", sep = ""))
 
 
 for (h in 1:2) {
@@ -192,8 +205,9 @@ for (h in 1:2) {
   # I propose to perform the analysis using the Ensembl identifier, if not the 
   # mean value of the genes with the same Symbol id should be performed.
   annot <- genome[which(genome$Ensembl %in% rownames(raw_counts)),]
-  gene_names <- annot[match(rownames(raw_counts), annot$Ensembl), -3]
+  gene_names <- annot[match(rownames(raw_counts), annot$Ensembl), ]
   print(dim(gene_names))
+  
   
   
   ################################################################################
@@ -244,8 +258,11 @@ for (h in 1:2) {
     metadata <- sample_info[which(sample_info[[trt]] %in% comp_lvl),]
     metadata[,trt] <- factor(metadata[,trt])
     
-    # Load gene count matrix
+    # Gene count matrix per comparison
     gene_counts <- raw_counts[, metadata$Sample]
+    
+    # Transformed data per comparison
+    res_log2 <- m_vst[which(rownames(m_vst %in% rownames(gene_counts))), metadata$Sample]
     
     # Select the contrast levels
     color_l <- color_list
@@ -387,220 +404,156 @@ for (h in 1:2) {
     dev.off()
     
     
+    ##############################################################################
+    #                            Data Processing
+    ##############################################################################
+  
+    
+    ## Results as a data frame
+    res_df <- res
+    res_df$Ensembl <- rownames(res_df)
+    print(dim(res_df))
+    
+    ## Threshold label
+    threshold <- paste("padj_", fdr_cutoff, "_log2FC_", round(lfc_cutoff, 2), sep ="")
+    
+    ## Significative genes
+    # Events with p-val NA are saved too
+    # Based on the alpha and log2 FC thresholds  
+    res_df$DEG[res_df$padj <= fdr_cutoff & abs(res_df$logFC) > lfc_cutoff] <- "YES"
+    res_df$DEG[res_df$padj > fdr_cutoff & abs(res_df$logFC) <= lfc_cutoff] <- "NO"
+    res_df$DEG[is.na(res_df$DEG)] <- "NO"
+    
+    ## Gene direction
+    # Contains if DEGs are up (Upregulated) or downregulated (Downregulated)
+    # Based on the alpha and log2 FC 
+    res_df$Direction[res_df$padj <= fdr_cutoff & res_df$logFC > lfc_cutoff] <- "Upregulated"
+    res_df$Direction[res_df$padj <= fdr_cutoff & res_df$logFC < -lfc_cutoff] <- "Downregulated"
+    res_df$Direction[res_df$padj > fdr_cutoff & res_df$logFC <= lfc_cutoff] <- "Not significant"
+    res_df$Direction[is.na(res_df$Direction)] <- "Not significant"
+    
+    
+    ## Annotated gene names in Symbol
+    res_df <- merge(gene_names, res_df, by = "Ensembl") 
+    print(head(res_df))
+    print(dim(res_df))
+    
+    ## MERGE WITH GENE COUNTS
+    # Row names to a variable
+    genes <- gene_counts
+    genes$Ensembl <-  rownames(genes)
+    # Merge gene_counts and comparison results
+    result <- merge(x = res_df, y = genes, by = "Ensembl")
+    
+    ## Differential expressed genes
+    # Select differentially expressed genes
+    df <- result[which(result$DEG == "YES"),]
+    dim(df)
+    
+    ## Transform matrix 
+    # Select the differentially expressed genes that overcame the test
+    # Used to plot the data 
+    m <- res_log2[which(rownames(res_log2) %in% df$Ensembl), ]
+    
+    if(identical(rownames(m), df$Ensembl) == FALSE){m <- m[match(rownames(m), df$Ensembl),]}
     
     
     
+    ##############################################################################
+    #                                 Plot
+    ##############################################################################
+    
+    ## HISTOGRAMS
+    # Representation of the adjusted p-value and log2 fold-change for all and 
+    # significant genes
+    plot_hist <- hist_verif(res_df, df)
+    ggsave(filename = paste("01_Histogram_verif_", ref, ".pdf", sep = ""), plot = plot_hist, path = dir_fig, height = 4, width = 4, bg = "white")
     
     
+    ##  CORRELATION 
+    #
+    # Execute a Pearson correlation which accept possible NAs.
+    # The acceptance of the NA should be valuable in the future in case, we accept
+    # comparisons when a sample is missing a PSI value.
+    #
+    # The results of the correlation matrix must be aligned with the results
+    # in the heatmap and PCA.
+    pem <- cor(m, method = "pearson", use = "na.or.complete")
+    plot_h <- pheatmap(pem, color = colorRampPalette(brewer.pal(9, "Blues"))(255),
+                       cluster_rows = TRUE, cluster_cols = TRUE, show_rownames = TRUE, show_colnames = TRUE,
+                       fontsize_row = 6, fontsize_col = 6, border_color = NA, treeheight_row = 0, treeheight_col = 0)
+    
+    pdf(paste(dir_fig, "/Cor_pearson_", ref, ".pdf", sep = ""), height = 4, width = 4, bg = "white")
+    print(plot_h)
+    dev.off()
+    
+    ## PCA PLOTS
+    plot_pcas <- pca_plot(m, trt, metadata, color_l)
+    
+    ggsave(filename = paste("PCA_params_", ref, ".pdf", sep = ""), plot = plot_pcas[[1]], path = dir_fig, height = 4, width = 4, bg = "white")
+    ggsave(filename = paste(deparse(substitute(pca_1vs2)), ref, ".pdf", sep = ""), plot = plot_pcas[[2]], path = dir_fig, height = 5, width = 6, bg = "white")
+    ggsave(filename = paste(deparse(substitute(pca_1vs3)), ref, ".pdf", sep = ""), plot = plot_pcas[[3]], path = dir_fig, height = 5, width = 6, bg = "white")
+    ggsave(filename = paste(deparse(substitute(pca_1vs4)), ref, ".pdf", sep = ""), plot = plot_pcas[[4]], path = dir_fig, height = 5, width = 6, bg = "white")
+    
+    ## HEATMAP
+    plot_heatmap <- heatmap_plot(m, metadata, trt, color_l)
+    
+    pdf(paste(dir_fig, "/Heatmap_zscore_", ref, ".pdf", sep = ""), height = 4, width = 4, bg = "white")
+    print(plot_heatmap)
+    dev.off()
+    
+    
+    ## VOLCANO
+    
+    volcano <- volcano_plot(res_df, color_list = color_l, lfc_cutoff, fdr_cutoff)
+    
+    ggsave(filename = paste("Volcano_", ref, ".pdf", sep = ""), plot = volcano[[1]], path = dir_fig, height = 5, width = 6, bg = "white")
+    ggsave(filename = paste("Volcano_color_", ref, ".pdf", sep = ""), plot = volcano[[2]], path = dir_fig, height = 5, width = 6, bg = "white")
+    
+    
+    ## WATERFALL
+    
+    waterfall_pl <- waterfall_plot(df, color_l)
+    waterfall_plot_top <- waterfall_top(df, color_l)
+    
+    ggsave(filename = paste("Waterfall_", ref, ".pdf", sep = ""), plot = waterfall_pl, path = dir_fig, height = 5, width = 6, bg = "white")
+    ggsave(filename = paste("Waterfall_top_genes_", ref, ".pdf", sep = ""), plot = waterfall_plot_top, path = dir_fig, height = 5, width = 6, bg = "white")
+    
+    
+    
+    ##############################################################################
+    #                               Save data 
+    ##############################################################################
+    
+    
+    # Summary table
+    sum_res <- c()
+    sum_res$Contrast <- name
+    sum_res$Method <- analysis
+    sum_res$Design <- design_cond
+    sum_res$Transformation <- md
+    sum_res$Genes <- dim(res_df$DEG)
+    sum_res$DEG <- sum(res_df$DEG == "YES")
+    sum_res$Up <- sum(res_df$Direction == "Upregulated")
+    sum_res$Down <- sum(res_df$Direction == "Downregulated")
+    write.csv(sum_res, paste(dir_output, "/Summary_tab_", analysis, ";", ref, "_", threshold,".csv", sep = ""))
+    
+    # All results
+    colnames(res_log2) <- paste(md, colnames(res_log2), sep = "_")
+    data <- cbind(result, res_log2)
+    data <- data %>% select(Name, Symbol, Ensembl, DEG, Direction, logFC, padj, MeanExp, lfcSE, stat, pvalue, everything())
+    
+    write.table(data, paste(dir_output, "/", ref, ";All_", md, "blindFALSE_", threshold,".txt", sep = ""), row.names = FALSE)
+    write.xlsx(data, paste(dir_output, "/", ref, ";All_", md, "blindFALSE_", threshold,".xlsx", sep = ""), overwrite = TRUE)
+    
+    # Differential expressed genes
+    colnames(m) <- paste(md, colnames(m), sep = "_")
+    sel <- cbind(df, m)
+    sel <- sel %>% select(Name, Symbol, Ensembl, DEG, Direction, logFC, padj, MeanExp, lfcSE, stat, pvalue, everything())
+    write.table(data, paste(dir_output, "/", ref, ";DEGs_", md, "blindFALSE", threshold,".txt", sep = ""), row.names = FALSE)  
 }
   
-
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  ##############################################################################
-  #                            Data Processing
-  ##############################################################################
-
-  
-  ## Results as a data frame
-  res_df <- res
-  res_df$Ensembl <- rownames(res_df)
-  print(dim(res_df))
-  
-  ## Threshold label
-  threshold <- paste("padj_", fdr_cutoff, "_log2FC_", round(lfc_cutoff, 2), sep ="")
-  
-  ## Significative genes
-  # Events with p-val NA are saved too
-  # Based on the alpha and log2 FC thresholds  
-  res_df$DEG[res_df$padj <= fdr_cutoff & abs(res_df$logFC) > lfc_cutoff] <- "YES"
-  res_df$DEG[res_df$padj > fdr_cutoff & abs(res_df$logFC) <= lfc_cutoff] <- "NO"
-  res_df$DEG[is.na(res_df$DEG)] <- "NO"
-  
-  ## Gene direction
-  # Contains if DEGs are up (Upregulated) or downregulated (Downregulated)
-  # Based on the alpha and log2 FC 
-  res_df$Direction[res_df$padj <= fdr_cutoff & res_df$logFC > lfc_cutoff] <- "Upregulated"
-  res_df$Direction[res_df$padj <= fdr_cutoff & res_df$logFC < -lfc_cutoff] <- "Downregulated"
-  res_df$Direction[res_df$padj > fdr_cutoff & res_df$logFC <= lfc_cutoff] <- "Not significant"
-  res_df$Direction[is.na(res_df$Direction)] <- "Not significant"
-  
-  
-  ## Annotated gene names in Symbol
-  res_df <- merge(gen_annot, res_df, by = "Ensembl") 
-  print(head(res_df))
-  print(dim(res_df))
-  
-  ## MERGE WITH GENE COUNTS
-  # Row names to a variable
-  genes <- gene_counts
-  genes$Ensembl <-  rownames(genes)
-  # Merge gene_counts and comparison results
-  result <- merge(x = res_df, y = genes, by = "Ensembl")
-  
-  ## Differential expressed genes
-  # Select differentially expressed genes
-  df <- result[which(result$DEG == "YES"),]
-  dim(df)
-  
-  
-  ## Variance stabilizing transformation
-  # 
-  # Variance stabilization methods in log2 scale to interpret the data
-  # 
-  # Choose VST for samples size group smaller than 30. Why? 
-  # 
-  # if you have many samples (e.g. 100s), the rlog function might take too long, and so the vst function will be 
-  # a faster choice. The rlog and VST have similar properties, but the rlog requires fitting a shrinkage term for 
-  # each sample and each gene which takes time. See the DESeq2 paper for more discussion on the differences 
-  # (Love, Huber, and Anders 2014)
-  
-  # Why blind = FALSE 
-  # 
-  
-  # Output
-  # - Columns are samples
-  # - Rows are genes
-  # 
-  #                    N_3_E1    N_2_E4    N_2_E3   N_2_E2   H4_2_E4   H4_2_E3   H4_2_E2   H4_2_E1
-  # ENSG00000000003 11.075692 10.993586 11.209109 11.03978 11.136152 11.147595 11.118555 11.125602
-  # ENSG00000000419 11.959761 11.938768 12.033994 11.84389 11.846617 11.951190 11.951552 11.888236
-  # ENSG00000000457 10.476816 10.557566 10.470185 10.47396 10.568444 10.488905 10.569326 10.394093
-  
-  # Estimate the biggest group sample size
-  group_n <- max(as.vector(tabulate(metadata[[trt]])))
-  
-  # Data transformation
-  if(group_n < 30){
-    res_log2 <- as.data.frame(assay(vst(dds, blind = FALSE)))
-    md <- "VST"
-  }else{
-    res_log2 <- as.data.frame(assay(rlog(dds, blind = FALSE)))
-    md <- "RLOG"}
-  print(md) 
-  
-  ## Transform matrix 
-  # Select the differentially expressed genes that overcame the test
-  # Used to plot the data 
-  m <- res_log2[which(rownames(res_log2) %in% df$Ensembl), ]
-  
-  if(identical(rownames(m), df$Ensembl) == FALSE){m <- m[match(rownames(m), df$Ensembl),]}
-  
-  
-  
-  ##############################################################################
-  #                                 Plot
-  ##############################################################################
-  
-  
-  ## HISTOGRAMS
-  # Representation of the adjusted p-value and log2 fold-change for all and 
-  # significant genes
-  plot_hist <- hist_verif(res_df, df)
-  ggsave(filename = paste("01_Histogram_verif_", ref, ".pdf", sep = ""), plot = plot_hist, path = dir_fig, height = 4, width = 4, bg = "white")
-  
-  
-  ##  CORRELATION 
-  #
-  # Execute a Pearson correlation which accept possible NAs.
-  # The acceptance of the NA should be valuable in the future in case, we accept
-  # comparisons when a sample is missing a PSI value.
-  #
-  # The results of the correlation matrix must be aligned with the results
-  # in the heatmap and PCA.
-  pem <- cor(m, method = "pearson", use = "na.or.complete")
-  plot_h <- pheatmap(pem, color = colorRampPalette(brewer.pal(9, "Blues"))(255),
-                     cluster_rows = TRUE, cluster_cols = TRUE, show_rownames = TRUE, show_colnames = TRUE,
-                     fontsize_row = 6, fontsize_col = 6, border_color = NA, treeheight_row = 0, treeheight_col = 0)
-  
-  pdf(paste(dir_fig, "/Cor_pearson_", ref, ".pdf", sep = ""), height = 4, width = 4, bg = "white")
-  print(plot_h)
-  dev.off()
-  
-  
-  ## PCA PLOTS
-  plot_pcas <- pca_plot(m, trt, metadata, color_l)
-  
-  ggsave(filename = paste("PCA_params_", ref, ".pdf", sep = ""), plot = plot_pcas[[1]], path = dir_fig, height = 4, width = 4, bg = "white")
-  ggsave(filename = paste(deparse(substitute(pca_1vs2)), ref, ".pdf", sep = ""), plot = plot_pcas[[2]], path = dir_fig, height = 5, width = 6, bg = "white")
-  ggsave(filename = paste(deparse(substitute(pca_1vs3)), ref, ".pdf", sep = ""), plot = plot_pcas[[3]], path = dir_fig, height = 5, width = 6, bg = "white")
-  ggsave(filename = paste(deparse(substitute(pca_1vs4)), ref, ".pdf", sep = ""), plot = plot_pcas[[4]], path = dir_fig, height = 5, width = 6, bg = "white")
-  
-  ## HEATMAP
-  plot_heatmap <- heatmap_plot(m, metadata, trt, color_l)
-  
-  pdf(paste(dir_fig, "/Heatmap_zscore_", ref, ".pdf", sep = ""), height = 4, width = 4, bg = "white")
-  print(plot_heatmap)
-  dev.off()
-  
-  
-  ## VOLCANO
-  
-  volcano <- volcano_plot(res_df, color_list = color_l, lfc_cutoff, fdr_cutoff)
-  
-  ggsave(filename = paste("Volcano_", ref, ".pdf", sep = ""), plot = volcano[[1]], path = dir_fig, height = 5, width = 6, bg = "white")
-  ggsave(filename = paste("Volcano_color_", ref, ".pdf", sep = ""), plot = volcano[[2]], path = dir_fig, height = 5, width = 6, bg = "white")
-  
-  
-  ## WATERFALL
-  
-  waterfall_pl <- waterfall_plot(df, color_l)
-  waterfall_plot_top <- waterfall_top(df, color_l)
-  
-  ggsave(filename = paste("Waterfall_", ref, ".pdf", sep = ""), plot = waterfall_pl, path = dir_fig, height = 5, width = 6, bg = "white")
-  ggsave(filename = paste("Waterfall_top_genes_", ref, ".pdf", sep = ""), plot = waterfall_plot_top, path = dir_fig, height = 5, width = 6, bg = "white")
-  
-  
-  ##############################################################################
-  #                               Save data 
-  ##############################################################################
-  
-  
-  # Summary table
-  sum_res <- c()
-  sum_res$Contrast <- name
-  sum_res$Method <- analysis
-  sum_res$Design <- design_cond
-  sum_res$Transformation <- md
-  sum_res$Genes <- dim(res_df$DEG)
-  sum_res$DEG <- sum(res_df$DEG == "YES")
-  sum_res$Up <- sum(res_df$Direction == "Upregulated")
-  sum_res$Down <- sum(res_df$Direction == "Downregulated")
-  write.csv(sum_res, paste(dir_output, "/Summary_tab_", analysis, ";", ref, "_", threshold,".csv", sep = ""))
-  
-  # All results
-  colnames(res_log2) <- paste(md, colnames(res_log2), sep = "_")
-  data <- cbind(result, res_log2)
-  data <- data %>% select(Name, Symbol, Ensembl, DEG, Direction, logFC, padj, MeanExp, lfcSE, stat, pvalue, everything())
-  
-  write.table(data, paste(dir_output, "/", ref, ";All_", md, "blindFALSE_", threshold,".txt", sep = ""), row.names = FALSE)
-  write.xlsx(data, paste(dir_output, "/", ref, ";All_", md, "blindFALSE_", threshold,".xlsx", sep = ""), overwrite = TRUE)
-  
-  # Differential expressed genes
-  colnames(m) <- paste(md, colnames(m), sep = "_")
-  sel <- cbind(df, m)
-  sel <- sel %>% select(Name, Symbol, Ensembl, DEG, Direction, logFC, padj, MeanExp, lfcSE, stat, pvalue, everything())
-  write.table(data, paste(dir_output, "/", ref, ";DEGs_", md, "blindFALSE", threshold,".txt", sep = ""), row.names = FALSE)
-
     }
 
  
@@ -629,6 +582,7 @@ log_data$min_total <- log_data$min_total
 log_data$fdr_cutoff <- fdr_cutoff
 log_data$lfc_cutoff <- lfc_cutoff
 log_data$correction <- correction
+log_data$Variance <- md
 log_data$contrast <- paste(unlist(contrast), collapse = ",")
 log_data$colortrt <- paste(color_list[[1]], collapse = ",")
 log_data$colorheat <- paste(color_list[[2]], collapse = ",")
